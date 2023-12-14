0e7a0cbc0b1919fb7388b26c34f5da587bb49b01
Kaworudesu <pedro.arcanjo2002@gmail.com>
2023-12-14 17:39:37 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,47 @@
+// @ts-ignore
+import Typical from 'react-typical';
+import logo from './assets/logo.png'
+import { useMediaQuery } from 'react-responsive';
+
+// @ts-ignore
+const MinWidthWrapper = ({ children }: ReactNode) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  return isMobile ? null : children;
+}
+
+function App(){
+  return(
+    <div className='min-h-screen min-w-full flex justify-around align-middle'>
+    <MinWidthWrapper>
+    <div className="bg-strongPink w-[100%] h-screen flex justify-center items-center invisible;">
+      <div className='introText flex font-medium text-3xl text-white p-4'>
+        <span>Conheça <span className='text-4xl font-bold'>GastronomIA</span> seu
+        <Typical
+          className='text-4xl font-bold'
+          steps={[' PARCEIRO', 3000, ' AMIGO', 3000, ' ASSISTENTE', 3000]}
+          loop={1}
+          wrapper={'span'}
+        />
+        de cozinha virtual!</span>
+      </div>
+    </div>
+    </MinWidthWrapper>
+    <div className='w-[50%] flex flex-col justify-center items-center'> 
+      <img src={logo} alt="logo" width={100} height={100} className='mb-2'/>
+      <h2 className='text-3xl mb-4'>Autenticação</h2>
+      <form className='flex flex-col gap-4 border p-5 rounded'>
+        <label>Email: </label>
+        <input type="text" className='border border-black rounded p-1'/>
+        <label>Senha: </label>
+        <input type="password" className='border border-black rounded p-1'/>
+        <button className='bg-medianPink rounded p-2 text-white font-semibold'>Entrar</button>
+      </form>
+      <br />
+      <a href='#'>Cadastrar-se</a>
+    </div>
+    </div>
+  )
+}
+
+export default App;
