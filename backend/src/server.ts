53564ac60a35c0b9b52dc4c06a988b5791de1fd2
Kaworudesu <pedro.arcanjo2002@gmail.com>
2024-01-16 19:44:13 -0300

    update: CORS sintaxe changes.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,22 @@
 import routes from "./routes";
+import { Request, Response, NextFunction } from "express";
 
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors')
+const corsOptions = {
+    origin: 'https://gastronom-ia.vercel.app/',
+    optionsSuccessStatus: 200,
+}
 
 const app = express();
 const PORT = process.env.PORT || 3336;
 
-app.use(cors());
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log('Middleware CORS está sendo chamado.');
+    cors()(req, res, next);
+});
+
+app.use(cors(corsOptions))
 // middleware para a configuração do JSON no express
 app.use(express.json());
 
