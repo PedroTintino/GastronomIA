86e7752128ed2405011d35cb0d3f96ecefb16daf
Kaworudesu <pedro.arcanjo2002@gmail.com>
2024-01-16 17:35:55 -0300

    fix: https requests changed to http ones.

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -18,7 +18,7 @@ function Auth(){
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
-  const baseUrl = 'https://64.23.149.242:3336'
+  const baseUrl = 'http://64.23.149.242:3336'
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault()
